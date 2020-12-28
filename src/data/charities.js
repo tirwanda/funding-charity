a372649cab2853c88c435f78e4055f01253dec58
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2020-12-28 18:01:22 +0700

    Adding HTML to Template Sveltejs

---
+++ src/data/charities.js
@@ -0,0 +1,17 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'money',
+            // thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            thumbnail: 'assets/images/causes/causes_13.jpg',
+            pledged: 9900,
+            target: 100000,
+            date_end: +new Date('10 January 2021'),
+            profile_photo: 'https://www.flickr.com/photos/z_a_r_a_11/50763647853/in/explore-2020-12-27/',
+            profile_name: 'tirwanda',
+            no_pledges: 0
+        }
+    ]
+};
